54c93fcf579f6a08d0ecb766005fe15ef07e045d
Ariel Gil <g.ariel22@gmail.com>
2025-03-16 16:07:27 -0400

    Update gsm8k.py

--- gsm8k.py
+++ gsm8k.py
@@ -26,3 +26,51 @@ def sample_to_fewshot(sample):
         + f"{sample.metadata['reasoning']}\n\n"
         + f"ANSWER" {sample.target}
     )
+
+MATH_PROMPT_TEMPLATE = """
+Solve the following math problem step by step. The last line of your
+response should be of the form "ANSWER: $ANSWER" (without quotes) 
+where $ANSWER is the answer to the problem.
+
+
+{prompt}
+
+Remember to put your answer on its own line at the end in the form
+"ANSWER: $ANSWER" (without quotes) where $ANSWER is the answer to 
+the problem, and you do not need to use a \\boxed command.
+
+Reasoning: 
+""".strip()
+
+@task
+def gsm8k(fewshot=10, fewshot_seed=42):
+    #build solver list dynamically (may or may not be using fewshot)
+    solver = [prompt_template(MATH_PROMPT_TEMPLATE), generate()]
+    if fewshot:
+        fewshots = hf_dataset(
+            path="gsm8k", 
+            data_dir="main",
+            split="train",
+            sample_fields=record_to_sample, 
+            shuffle=True,
+            seed=fewshot_seed,
+            limi=fewshot,
+        )
+        solver.insert(
+            0, 
+            system_message(
+                "\n\n".join([sample_to_fewshot(sample) for sample in fewshots])
+                ),
+            )
+    
+## define task 
+    return Task(dataset=hf_dataset(
+        path="gsm8k",
+        data_dir="main",
+        split="test",
+        sample_fields=record_to_sample,
+    ),
+    solver=solver,
+    scorer=match(numeric=True),
+)
+
